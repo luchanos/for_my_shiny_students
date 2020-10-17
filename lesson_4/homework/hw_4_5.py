ec3d31d1fe1689f1a6f6025abfb4231430181115
nnsviridov <nnsviridov@domclick.ru>
2020-10-17 18:54:21 +0300

    [upd] Домашка к четвертому уроку.

---
+++ lesson_4/homework/hw_4_5.py
@@ -0,0 +1,15 @@
+"""
+5. Реализовать формирование списка, используя функцию range() и возможности генератора.
+В список должны войти четные числа
+ от 100 до 1000 (включая границы). Необходимо получить результат вычисления произведения всех элементов списка.
+Подсказка: использовать функцию reduce().
+"""
+from functools import reduce
+
+
+def mul_list(el_1, el_2):
+    return el_1 * el_2
+
+
+uniq_list = [el for el in range(100, 1001, 2)]
+print(f"List\n{uniq_list}\nMultiplication of numbers\n{reduce(mul_list, uniq_list)}")
