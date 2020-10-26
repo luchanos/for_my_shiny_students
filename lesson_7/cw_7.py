096d3724056eb125594cb0cde4a4e4c2d9fd32cf
nnsviridov <nnsviridov@domclick.ru>
2020-10-26 20:45:40 +0300

    [upd] Классная работа седьмого урока.

--- lesson_7/cw_7.py
+++ lesson_7/cw_7.py
@@ -126,3 +126,150 @@ class User:
     @property
     def get_discount_status(self):
         return requests.get("https://google.com")
+
+
+
+###################################
+############ ИТЕРАТОРЫ ############
+###################################
+
+import sys
+from itertools import repeat, count
+
+# Итерируемые объекты (iterable) — это любые объекты, предоставляющий возможность поочерёдного прохода по циклу.
+from time import sleep
+
+my_list = [1, 2, 3]
+my_dict = {1: "a", 2: "b", 3: "c"}
+my_set = {"lol", "kek", "cheburek"}
+
+
+# простая петля
+# for element in my_set:
+#     print(element)
+
+# for element in my_dict:
+#     print(element)
+
+# iterator = iter(my_list)
+# print(next(my_list))  # упадёт ошибка
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+
+# numbers = [1, 2, 3, 4, 5, 6]
+# squares = (n**2 for n in numbers)
+# print(9 in squares)
+# print(9 in squares)
+# print(next(squares))
+
+
+def simple_gen(start, finish=5):
+    while start <= finish:
+        yield start
+        start += 1
+
+
+class MyShinyIterator:
+    def __init__(self, start):
+        self.i = start
+        # self.buf = start
+
+    def __next__(self):
+        self.i += 1
+        if self.i <= 5:
+            return self.i
+        else:
+            raise StopIteration("Итерация окончена")
+
+    # def to_start(self):
+    #     self.i = self.buf
+
+    def __iter__(self):
+        c = 1
+        return self
+
+
+class TumbCollection:
+    """
+    Объект, поддерживающий интерфейс итерации (итерируемый объект)
+    """
+    def __init__(self, start):
+        self.start = start - 1
+
+    def __iter__(self):
+        c = 1
+        # Метод __iter__ должен возвращать объект-итератор (генератор - это подвид итератора)
+        return simple_gen(self.start)
+
+
+# Ключевой вопрос - что можно итерировать? Ответ - то, что реализует интерфейс итерации.
+# obj = TumbCollection(start=1)
+# for el in obj:
+#     print(el)
+# print("тумб второй проход")
+# for el in obj:
+#     print(el)
+
+# print("************")
+# it = MyShinyIterator(start=2)
+# for el in it:
+#     print(el)
+# print("второй проход")
+# for el in it:
+#     print(el)
+# print("третий проход")
+# for el in it:
+#     print(el)
+
+# gen = simple_gen(5, 7)
+# for x in gen:
+#     print(x)
+# for x in gen:
+#     print(x)
+# for x in gen:
+#     print(x)
+# print(gen.__next__())
+# print(gen.__next__())
+# print(gen.__next__())
+# print(gen.__next__())
+
+
+class SquareAll:
+    def __init__(self, numbers):
+        self.numbers = iter(numbers)
+
+    def __next__(self):
+        return next(self.numbers) ** 2
+
+    def __iter__(self):
+        return self
+
+
+numbers = [1, 2, 3, 4, 5]
+
+
+def square_all(numbers):
+    for n in numbers:
+        yield n**2
+
+
+square_all_ex = (n**2 for n in numbers)
+print(9 in square_all_ex)
+print([x for x in square_all_ex])
+print(9 in square_all_ex)
+
+
+# как мы экономим память
+# lots_of_fours = repeat(4, times=100_000_000)
+# print(type(lots_of_fours))
+# print(f"lots_of_fours занимает в памяти {sys.getsizeof(lots_of_fours)} байт")
+
+# lots_of_fours_via_list = [4] * 100_000_000
+# print(f"lots_of_fours занимает в памяти {sys.getsizeof(lots_of_fours_via_list)} байт")
+
+# бесконечно длинный итератор
+# for n in count():
+#     print(n)
+#     sleep(1)
