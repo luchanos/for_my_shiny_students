cf0e25513f0c9111219ee1c9a219bffbb2b9a586
nnsviridov <nnsviridov@domclick.ru>
2020-11-01 12:54:21 +0300

    [upd] Классная работа первого урока.

---
+++ lesson_1/cw_1.py
@@ -0,0 +1,35 @@
+
+a = 123456789
+b = False
+c = "stroka"
+
+# print(a % 1000)
+
+# h = not a % 2 == 0
+# print(h)
+
+#### УСЛОВНЫЙ ОПЕРАТОР ####
+# if a % 2 != 0 and c == "stroka" and not b:
+#     print("its good")
+# else:
+#     print(1)
+from time import sleep
+
+#### ЦИКЛ WHILE ####
+# cnt = 0
+# while True:
+#     sleep(.1)
+#     cnt += 1
+#     if cnt >= 10:
+#         break
+# print("цикл закончен")
+
+
+
+#### ИНТЕРПРЕТИРУЕМОСТЬ И ДИНАМИЧЕСКАЯ ТИПИЗАЦИЯ ####
+# print("Start of program")
+# # c = 1 / 0
+# c = 1
+# print(c)
+# c = 'sdfsdfsdf'
+# print(c)
