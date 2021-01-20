678a7d2d45d53f06fb45f85905d0bba1d9456dcc
nnsviridov <nnsviridov@domclick.ru>
2021-01-20 22:31:45 +0300

    [upd] refactoring.

--- oop.py
+++ oop.py
@@ -1,9 +1,9 @@
 from uuid import uuid4
 from abc import ABC, abstractmethod
-import requests
 
 
 class Ticket(ABC):
+
     def __init__(self):
         self.valid = True
         self.ticket_id = uuid4()
@@ -17,26 +17,68 @@ class Ticket(ABC):
         pass
 
     @abstractmethod
-    def notify_host(self, *args, **kwargs):
+    def _notify_host(self, *args, **kwargs):
         pass
 
     def show_ticket_primary_info(self):
         return {"valid": self.valid, "ticket_id": self.ticket_id}
 
+    @staticmethod
+    def some_func(a, b, c):
+        print("вывожу информацию по истории возникновения билетов")
 
-class TheaterTicket(Ticket):
-    e_mail = "some-email@lolkekcheburek.com"
+
+class EmailClient:
+    def __init__(self, email):
+        self.email = email
+
+    def _send_email(self, data):
+        print("отправил письмо")
+
+    def __call__(self, data, *args, **kwargs):
+        self._send_email(data)
+
+
+class AnnulException(Exception):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+
+class UseMeException(Exception):
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+
+class TheaterTicketAbstract(Ticket, ABC):
+    e_mail: str
+    sender_email_client: EmailClient
 
     def use_me(self):
         self.valid = False
         print("Даём человеку бинокль в подарок")
+        raise UseMeException("Ошибка", [1, 2, 3], 123, 'adfgsdfg')
 
     def annul(self, reason):
         self.valid = False
         print(f"Билет id {self.ticket_id} был аннулирован по причине {reason}")
+        self.__class__._notify_host("data")
+        raise AnnulException("Ошибка", [1, 2, 3], 123, 'adfgsdfg')
 
-    def notify_host(self, data):
-        print(f"Отправляю данные {data} на адрес почты {TheaterTicket.e_mail}")
+
+    @classmethod
+    def _notify_host(cls, data):
+        print(f"Отправляю данные {data} на адрес почты {cls.sender_email_client.email}")
+        cls.sender_email_client(data)
+
+
+class TheaterTicket(TheaterTicketAbstract):
+    sender_email_client = EmailClient("some-email@lolkekcheburek.com")
+
+
+class TheaterTicketBranch(TheaterTicketAbstract):
+    sender_email_client = EmailClient("some@lol.com")
 
 
 class FlightTicket(Ticket):
@@ -49,6 +91,9 @@ class FlightTicket(Ticket):
     def annul(self, reason):
         self.valid = False
         print(f"Билет id {self.ticket_id} был аннулирован по причине {reason}")
+
+    def _notify_host(self, data):
+        print(f"Отправляю POST запрос с данными {data} на адрес {FlightTicket.url}")
 
 
 class Controller:
@@ -72,5 +117,13 @@ class Controller:
 
 controller = Controller(1)
 t_ticket = TheaterTicket()
-f_ticket = FlightTicket()
-controller.businees_logic("событие_1", f_ticket)
+# t_ticket.annul("причина")
+# t2 = TheaterTicketBranch()
+# t2.annul("другая причина")
+# f_ticket = FlightTicket()
+try:
+    controller.businees_logic("событие_1", t_ticket)
+except AnnulException as err:
+    print(err)
+# controller.businees_logic("событие_2", t2)
+# Ticket.some_func(1, 2, 3)
