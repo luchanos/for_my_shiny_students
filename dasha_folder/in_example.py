9bfa0434441c2f229671950537a4313e74b406e0
nnsviridov <nnsviridov@domclick.ru>
2021-04-04 21:46:20 +0100

    [upd] Даша первый урок

---
+++ dasha_folder/in_example.py
@@ -0,0 +1,42 @@
+h = "Даша, привет!"
+h_1 = "Как дела?"
+# print(h)
+# print(print)
+# print(h_1)
+c = 123
+f = 123.5
+b = False
+bt = b'sadfasdfasdfas'
+wrfqefqefqewqefqerfqerfqerfeqrferfqer = 1232
+j = 1
+
+# print(int, bool, float, str)
+# a = int(100 / 2)
+# p = bool()
+# i = float()
+# o = str()
+# print(a, p, o)
+# print(100 / 2)
+
+# res = 100 / 3
+# print(res)
+# print(int(res))
+# b = True
+# print(int(b))
+# print(float(b))
+
+# b = False
+# print(int(b))
+# print(float(b))
+
+# i = 0.01
+# print(bool(i))
+
+s = "test string"
+s_1 = 'test string 2'
+s3 = ''
+s4 = '123.3'
+# print(s, s_1, sep=', ', end='.\n')
+# print(s + ', ' + s_1 + '.')
+# print(bool(s3))
+print(type(float(s4)), float(s4), type(s4), s4)
