d5ac24306c6131de6245d0d7b951c1ef3d7cfebc
luchanos <lukash_91@mail.ru>
2021-06-28 11:51:09 +0300

    задачки codewars

--- dasha_folder/codewars.py
+++ dasha_folder/codewars.py
@@ -1,3 +1,17 @@
 """
+ЧИСЛА:
+
+Квартал года:
+https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/
+
+Среднее значение массива:
 https://www.codewars.com/kata/563e320cee5dddcf77000158/python
+
+Возврат отрицательного числа:
+https://www.codewars.com/kata/55685cd7ad70877c23000102
+
+
+СТРОКИ
+Строка с приветствием:
+https://www.codewars.com/kata/55a70521798b14d4750000a4/solutions/python
 """
